512b2f1f0036a57ad1719e025465c48fef2a6eaf
luisduenas <luis.alonso.duenas.m@gmail.com>
2018-12-13 12:00:20 -0800

    Added jQuery Validation Bootstrap customization

---
+++ src/Treehouse.FitnessFrog/Scripts/jquery.validate.bootstrap.js
@@ -0,0 +1,23 @@
+﻿(($) => {
+    const defaultOptions = {
+        validClass: 'has-success',
+        errorClass: 'has-error',
+        highlight: (element, errorClass, validClass) => {
+            $(element).closest('.form-group')
+                .removeClass(validClass)
+                .addClass(errorClass);
+        },
+        unhighlight: (element, errorClass, validClass) => {
+            $(element).closest('.form-group')
+                .removeClass(errorClass)
+                .addClass(validClass);
+        }
+    };
+
+    $.validator.setDefaults(defaultOptions);
+
+    $.validator.unobtrusive.options = {
+        errorClass: defaultOptions.errorClass,
+        validClass: defaultOptions.validClass,
+    };
+})(jQuery);
